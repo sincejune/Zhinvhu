614b7b2101bfce4c098a05ecad3b0ccb6ccb4829
oleooo <oleooo@126.com>
2016-05-23 19:42:44 +0800

    update pages

--- src/Zhinvhu.py
+++ src/Zhinvhu.py
@@ -1,13 +1,14 @@
 #! /usr/bin/env python3
 #-*- coding:utf-8 -*-
 import os
+import re
 from bs4 import BeautifulSoup
 from PIL import Image
 import requests
 import json
 import time
 
-sum = 1;
+
 
 headers_base = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
@@ -16,12 +17,16 @@ headers_base = {
     'Connection': 'keep-alive',
     'Host': 'www.zhihu.com',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36', 
-    'Referer': 'https://www.zhihu.com/',
+    'Referer': 'https://www.zhihu.com/'
 }
 
+current = 1
+sum = 1
+total = 1
 path = "e:\\zhihu\\"
 file = "e:\\pic.txt"
 base = "https://www.zhihu.com"
+url = "https://www.zhihu.com/collection/26815754"
 
 def is_path_exist():
     if not os.path.exists(path):
@@ -37,8 +42,19 @@ def is_file_exist():
         finally:
             f.close()  
 
+def count_pages(url):
+    global total
+    r = requests.get(url,headers = headers_base)
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.text,"lxml")
+        pages = soup.find_all(href=re.compile("^\?page=\d{1,3}")) #1-999
+        pattern = re.compile("\d{1,3}")
+        for page in pages:
+            match = pattern.match(page.contents[0])
+            if match:
+                total = page.contents[0]
 
-#define  find_what
+#define find_what
 def download(url):
     print(url)
     d = requests.get(url,headers=headers_base)
@@ -54,31 +70,30 @@ def download(url):
 def save_img(url):
     img = requests.get(url)
     if img.status_code == 200:
-        with open(path+url.split('/')[-1],'wb' ) as f:
+        with open(path + url.split('/')[-1],'wb') as f:
             f.write(img.content)
             f.close()
     else:
-        print(url+" 404 not founded")
+        print(url + " 404 not founded")
      
 
-
-
-def has(tag):
-    return tag.has_attr("href") and tag.has_attr("class")
-
-def main(url):
-    is_path_exist()
-    is_file_exist()
+def down_page(url):
+    global current
+    print("====================PAGE" + str(current) + "===================")
     r = requests.get(url,headers = headers_base)
     if r.status_code == 200:
-        #r.decode("utf-8")
-        #print(t.text)
         soup = BeautifulSoup(r.text,"lxml")
         tag = soup.find_all(href=True,class_="toggle-expand")
         for x in tag:
             #time.sleep(5)
-            download(base+x['href'])
+            download(base + x['href'])
+        current = current + 1
+        if current <= int(total):
+            down_page(url + "?\page=" + str(current))
     
 if __name__ == '__main__':
-    main("https://www.zhihu.com/collection/26815754")
+    is_path_exist()
+    is_file_exist()
+    count_pages(url)
+    down_page(url)
     
